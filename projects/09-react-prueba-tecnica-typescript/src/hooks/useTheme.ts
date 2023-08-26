3b7bfd3e8a3b149f3f1a70d2c080c020e8e35169
luisllaboj <luisllaboj@gmail.com>
2023-08-26 00:40:35 -0700

    refactorizando

--- projects/09-react-prueba-tecnica-typescript/src/hooks/useTheme.ts
+++ projects/09-react-prueba-tecnica-typescript/src/hooks/useTheme.ts
@@ -1,46 +1,47 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-type Theme = 'light' | 'dark' | 'system'
-
-// export type Theme = 'light' | 'dark' | 'system'
+type Theme = "light" | "dark" | "system";
 
 export default function useTheme() {
   const [theme, setTheme] = useState<Theme>(() => {
-    return (localStorage.getItem('theme') as Theme) || 'system'
-  })
+    return (localStorage.getItem("theme") as Theme) || "system";
+  });
 
   useEffect(() => {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    );
     const handleThemeChange = () => {
       if (
-        localStorage.theme === 'dark' ||
-        (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
       ) {
-        document.documentElement.classList.add('dark')
+        document.documentElement.classList.add("dark");
       } else {
-        document.documentElement.classList.remove('dark')
+        document.documentElement.classList.remove("dark");
       }
-    }
+    };
 
-    if (theme === 'dark') localStorage.theme = 'dark'
-    else if (theme == 'light') localStorage.theme = 'light'
+    if (theme === "dark") localStorage.theme = "dark";
+    else if (theme == "light") localStorage.theme = "light";
     else {
-      localStorage.removeItem('theme')
-      console.log('Setando darkModeMediaQuery listener')
-      darkModeMediaQuery.addEventListener('change', handleThemeChange)
+      localStorage.removeItem("theme");
+      console.log("Setando darkModeMediaQuery listener");
+      darkModeMediaQuery.addEventListener("change", handleThemeChange);
     }
 
-    handleThemeChange()
+    handleThemeChange();
 
     return () => {
-      console.log('Limpiando darkModeMediaQuery listener')
-      darkModeMediaQuery.removeEventListener('change', handleThemeChange)
-    }
-  }, [theme])
+      console.log("Limpiando darkModeMediaQuery listener");
+      darkModeMediaQuery.removeEventListener("change", handleThemeChange);
+    };
+  }, [theme]);
 
-  const setDarkMode = () => setTheme('dark')
-  const setLightMode = () => setTheme('light')
-  const setDefaultSystemMode = () => setTheme('system')
+  const setDarkMode = () => setTheme("dark");
+  const setLightMode = () => setTheme("light");
+  const setDefaultSystemMode = () => setTheme("system");
 
-  return { theme, setDarkMode, setLightMode, setDefaultSystemMode }
+  return { theme, setDarkMode, setLightMode, setDefaultSystemMode };
 }
