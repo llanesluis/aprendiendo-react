af9de1f8399a43edc17bbfa4eb2da27f35d74140
luisllaboj <luisllaboj@gmail.com>
2023-08-29 23:57:44 -0700

    Header

--- projects/10-quiz-con-zustand/src/App.tsx
+++ projects/10-quiz-con-zustand/src/App.tsx
@@ -1,7 +1,25 @@
+import { Container, Stack, Typography } from '@mui/material'
 import './App.css'
+import { JavaScriptLogo } from './components/Icons'
 
 function App() {
-  return <h1>Javascript quizz</h1>
+  return (
+    <>
+      <header>
+        <Container maxWidth={'sm'}>
+          {/* El stack sirve para agrupar un conjunto de elementos */}
+          <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+            <JavaScriptLogo />
+            {/* component es para indicar que renderizar y variant para indicar como se ve */}
+            <Typography component='h1' variant='h2'>
+              JavaScript Quizz
+            </Typography>
+          </Stack>
+        </Container>
+      </header>
+      <main></main>
+    </>
+  )
 }
 
 export default App
